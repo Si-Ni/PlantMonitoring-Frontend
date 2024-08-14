b842880a0bdbf2146cebba093c856c1f159ecaf2
MaRcR11 <MaRcR11@users.noreply.github.com>
2024-08-14 21:59:44 +0000

    Prettified Code!

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://plantmonitoringapi.azurewebsites.net/api",
+  baseURL: "https://plantmonitoringapi.azurewebsites.net/api"
 });

36bcd1a330c7bad7a285323ccd338c2774a36dfd
MaRcR11 <marcr.11@t-online.de>
2024-08-15 10:02:38 +0200

    added basic login page

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,16 @@ import ReactDOM from "react-dom/client";
 import { NextUIProvider } from "@nextui-org/react";
 import App from "./App";
 import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <NextUIProvider>
     <main className="dark text-foreground">
-      <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+  </BrowserRouter>
     </main>
   </NextUIProvider>
 );
---
+++ src/pages/Login.tsx
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+import { Input, Button,  Card, CardBody } from "@nextui-org/react";
+import { EyeSlashFilledIcon } from '../components/EyeSlashFilledIcon';
+import { EyeFilledIcon } from '../components/EyeFilledIcon';
+
+function Login() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => setIsVisible(!isVisible);
+
+  const handleSubmit = () => {
+    console.log({ email, password });
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <Card className="w-full max-w-md p-6 bg-content">
+        <CardBody className="flex flex-col items-center">
+          <h2 className="text-2xl bg-gradient-to-r from-primary via-pink-500 to-danger bg-clip-text text-transparent mb-6">
+            PlantMonitoring <span role="img" aria-label="wave">🌱</span>
+          </h2>
+          <Input
+            type="email"
+            label="Email"
+            variant="bordered"
+            className="mb-4 w-full"
+            placeholder="Enter your Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            label="Password"
+            variant="bordered"
+            placeholder="Enter your password"
+            endContent={
+              <button
+                className="focus:outline-none"
+                type="button"
+                onClick={toggleVisibility}
+                aria-label="toggle password visibility"
+              >
+                {isVisible ? (
+                  <EyeSlashFilledIcon className="text-xl text-gray-400 pointer-events-none" />
+                ) : (
+                  <EyeFilledIcon className="text-xl text-gray-400 pointer-events-none" />
+                )}
+              </button>
+            }
+            type={isVisible ? "text" : "password"}
+            className="mb-8 w-full"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button onPress={handleSubmit} color="primary" className="w-full mb-4 mt-6">
+            Log In
+          </Button>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
+
+export default Login;
